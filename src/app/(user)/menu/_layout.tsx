75778aa65c9a6ecfea7f4c100e38ac645f4acdf1
Catevika <dominique.bello@outlook.fr>
2024-12-01 09:59:50 +0100

    Fix Android Modal issue

--- src/app/(user)/menu/_layout.tsx
+++ src/app/(user)/menu/_layout.tsx
@@ -1,27 +1,26 @@
 import { useColorScheme } from '@/components/useColorScheme';
 import Colors from '@/constants/Colors';
 import { FontAwesome } from '@expo/vector-icons';
-import { Link, Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { Pressable } from 'react-native';
 
 export default function MenuStack() {
   const colorScheme = useColorScheme();
+  const router = useRouter();
 
   return (
     <Stack screenOptions={{
       headerRight: () => (
-        <Link href="/cart" asChild>
-          <Pressable>
-            {({ pressed }) => (
-              <FontAwesome
-                name="shopping-cart"
-                size={25}
-                color={Colors[colorScheme ?? 'light'].tint}
-                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-              />
-            )}
-          </Pressable>
-        </Link>
+        <Pressable onPressIn={() => router.push('/cart')}>
+          {({ pressed }) => (
+            <FontAwesome
+              name="shopping-cart"
+              size={25}
+              color={Colors[colorScheme ?? 'light'].tint}
+              style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+            />
+          )}
+        </Pressable>
       ),
     }}>
       <Stack.Screen name='index' options={{ title: 'Menu' }} />
--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,15 +1,19 @@
 import products from '@/assets/data/products';
 import ProductListItem from '@/components/ProductListItem';
+import { StatusBar } from 'expo-status-bar';
 import { FlatList } from 'react-native';
 export default function MenuIndex() {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-      keyExtractor={(item) => item.id.toString()}
-      numColumns={2}
-      contentContainerStyle={{ gap: 10, padding: 10 }} // Gap in row + padding around the whole flat list
-      columnWrapperStyle={{ gap: 10 }} // Gap in column
-    />
+    <>
+      <StatusBar style='auto' />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }} // Gap in row + padding around the whole flat list
+        columnWrapperStyle={{ gap: 10 }} // Gap in column
+      />
+    </>
   );
 }

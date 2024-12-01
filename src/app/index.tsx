59ceab2d23ad18281bffbf06c47170c1eb46f0d4
Catevika <dominique.bello@outlook.fr>
2024-12-01 16:40:07 +0100

    Add Auth, Sign in and Sign up UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,6 +13,9 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link>
+      <Link href={'/(auth)/sign-in'} asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 };
